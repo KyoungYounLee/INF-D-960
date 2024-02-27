3de51bedb77b3f6b68dbff6fb20a1ecf06136d37
Kyoung Youn Lee <kyoung_youn.lee@mailbox.tu-dresden.de>
2024-02-27 14:27:53 +0100

    Add a method to find t1, t2 queries

--- src/optimizer/optimizer.py
+++ src/optimizer/optimizer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Optional
 
 from postbound.qal.relalg import RelNode, SubqueryScan
 
@@ -7,22 +7,24 @@ class Optimizer:
     def __init__(self):
         pass
 
-    def optimize_unnesting(self, relalg: RelNode) -> RelNode:
+    def optimize_unnesting(self, relalg: RelNode):
         """
         Checken ob die Query einen abhängigen Join hat.
-        Abhängigen Join umwandeln
         Subquery (T2), Outerquery (T1) berechnen
         D berechnen
         Abhängigen Join in Unnesting-Form umwandeln
         """
 
-        localRelNodes = self._find_dependent_subquery_node(relalg)
-        if len(localRelNodes) > 0:
-            return localRelNodes[0]
+        local_rel_nodes = self._find_dependent_subquery_node(relalg)
+        if len(local_rel_nodes) == 0:
+            return relalg
 
-        return relalg
+        t1, t2 = self._derive_sub_and_outer_queries(local_rel_nodes[0])
 
-    def _find_dependent_subquery_node(self, relalg: RelNode) -> List[RelNode]:
+        return t1, t2
+
+    @staticmethod
+    def _find_dependent_subquery_node(relalg: RelNode) -> List[SubqueryScan]:
         """
         This function finds and returns dependent join and subjoin within the given relational algebra representation (relalg).
 
@@ -41,11 +43,19 @@ class Optimizer:
         find_dependent_subqueries(relalg)
         return subqueries
 
-    def _transform_dependent_join(self):
-        pass
+    @staticmethod
+    def _derive_sub_and_outer_queries(subquery: SubqueryScan) -> Tuple[Optional[RelNode], RelNode]:
+        t2 = subquery.input_node
 
-    def _derive_sub_and_outer_queries(self):
-        pass
+        parent_node = subquery.parent_node
+        t1 = None
+
+        if parent_node:
+            for child in parent_node.children():
+                if child != t2:
+                    t1 = child
+                    break
+        return t1, t2
 
     def _derive_domain_D(self):
         pass
