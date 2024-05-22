fb529976655817a2406657fab0472bd1e18be620
Kyoung Youn Lee <kyoung_youn.lee@mailbox.tu-dresden.de>
2024-05-22 13:39:11 +0200

    generate sql subquery

--- src/query_generator/query_generator.py
+++ src/query_generator/query_generator.py
@@ -33,16 +33,23 @@ class QueryGenerator:
                                                                additional_relations=[
                                                                    TableReference("outerquery", "oq")])
         # 3-1) den leeren Join in 3) mit dem rechten Kindknoten des ersten Joins füllen
+        sql_sub_query, agg_mapping = self._generate_sub_query(first_join.right_input,
+                                                              stop_node=next(iter(
+                                                                  first_join.left_input.sideways_pass)).parent_node,
+                                                              domain_columns=domain_columns_name)
+
         # 3-2) Die ON-Bedingung nach dem Join in 3) sollte das Prädikat des ersten Joins sein.
         #      Im Prädikat 'd' mit 'oq' ersetzen und den Rest mit 'subquery'.
         sql_join_predicate = str(self._extract_join_predicate(first_join))
         sql_main_query_with_join = self._add_join_to_query(sql_main_query, "() as subquery", sql_join_predicate)
-        sql_main_query_renamed_subquery = self._rename_subquery_columns(sql_main_query_with_join)
+        sql_main_query_renamed_subquery = self._rename_subquery_columns(sql_main_query_with_join, agg_mapping)
         sql_main_query_without_subquery = self._rename_columns_in_main_query(sql_main_query_renamed_subquery,
                                                                              outerquery_relations)
 
+        sql_main_and_sub_query = re.sub(r'JOIN \(\)', f'JOIN (\n\t{sql_sub_query})', sql_main_query_without_subquery)
+
         # 1, 2, 3 in einer Zeichenkette zusammenführen und zurückgeben
-        sql_final = f"{sql_outerquery},\n{sql_dup_elim}\n{sql_main_query_without_subquery}"
+        sql_final = f"{sql_outerquery},\n{sql_dup_elim}\n{sql_main_and_sub_query}"
         return sql_final
 
     @staticmethod
@@ -55,8 +62,10 @@ class QueryGenerator:
             queue.extend(current.children())
 
     def _generate_outer_query(self, node: RelNode) -> (str, list):
-        inner_query, relations = self._generate_simple_select_query(node, self.utils.find_all_dependent_columns)
-        outer_query = f"WITH outerquery AS ({inner_query})"
+        inner_query, relations = self._generate_simple_select_query(node,
+                                                                    column_generator=self.utils.find_all_dependent_columns,
+                                                                    tab_line=True)
+        outer_query = f"WITH outerquery AS (\n\t{inner_query})"
 
         return outer_query, relations
 
@@ -77,14 +86,79 @@ class QueryGenerator:
 
         return transform.rename_columns_in_predicate(join_node.predicate, column_mapping)
 
-    def _generate_main_query(self, node: RelNode) -> str:
-        return ""
+    def _generate_sub_query(self, node: RelNode, *, stop_node: RelNode = None, domain_columns=None) -> (str, list):
+        select_part = "SELECT "
+        from_part = "FROM "
+        where_part = ""
+        groupby_part = ""
+        queue = deque([node])
 
-    def _generate_sub_query(self, node: RelNode, sql_query: str) -> str:
-        return ""
+        relations = []
+        where_conditions = []
+        agg_mapping = {}
+        agg_count = 1
+        has_group_by = False
 
-    def _generate_simple_select_query(self, node: RelNode, column_generator=None, stop_node=None,
-                                      additional_relations: [TableReference] = None) -> (str, list):
+        dup_elim_outerquery = TableReference("dup_elim_outerquery", "d")
+        relations.append(dup_elim_outerquery)
+
+        while queue:
+            current = queue.popleft()
+
+            if isinstance(current, Relation):
+                relations.append(current.table)
+            elif isinstance(current, Projection):
+                columns = []
+                for col in current.columns:
+                    col_str = str(col)
+                    if re.search(r'\b(AVG|SUM|COUNT|MIN|MAX)\b', col_str, re.IGNORECASE):
+                        alias = f"m{agg_count}"
+                        columns.append(f"{col_str} AS {alias}")
+                        agg_mapping[col_str] = alias
+                        agg_count += 1
+                    else:
+                        columns.append(col_str)
+
+                select_part += ', '.join(columns)
+            elif isinstance(current, GroupBy):
+                has_group_by = True
+                grouping_columns = ', '.join([str(col) for col in current.group_columns])
+                if grouping_columns:
+                    groupby_part = f"GROUP BY {grouping_columns}"
+
+            elif isinstance(current, (ThetaJoin, Selection)):
+                where_conditions.append(str(current.predicate))
+
+            if not stop_node or stop_node not in current.children():
+                queue.extend(current.children())
+            else:
+                children_without_stop_node = [child for child in current.children() if child != stop_node]
+                queue.extend(children_without_stop_node)
+
+        where_part = "WHERE " + " AND ".join(where_conditions) if where_conditions else ""
+        from_part += ", ".join(
+            [f"{relation.full_name} {relation.alias}" for relation in relations if relation.full_name != "DummyTable"])
+
+        # Todo: Muss geändert werden, sobald die Pushdown-Regel abgeschlossen ist
+        if domain_columns:
+            domain_columns_with_prefix = [f"d.{col}" for col in domain_columns]
+            if select_part.endswith("SELECT "):
+                select_part += ', '.join(domain_columns_with_prefix)
+            else:
+                select_part += ', ' + ', '.join(domain_columns_with_prefix)
+
+            if has_group_by:
+                if groupby_part.startswith("GROUP BY "):
+                    groupby_part += ', ' + ', '.join(domain_columns_with_prefix)
+                else:
+                    groupby_part += 'GROUP BY ' + ', '.join(domain_columns_with_prefix)
+
+        sql_query = f"{select_part}\n\t{from_part}\n\t{where_part}\n\t{groupby_part}"
+
+        return sql_query.strip(), agg_mapping
+
+    def _generate_simple_select_query(self, node: RelNode, *, column_generator=None, stop_node=None,
+                                      additional_relations: [TableReference] = None, tab_line=False) -> (str, list):
         select_part = "SELECT "
         from_part = "FROM "
         where_part = ""
@@ -112,7 +186,7 @@ class QueryGenerator:
             elif isinstance(current, GroupBy):
                 grouping_columns = ', '.join([str(col) for col in current.group_columns])
                 if grouping_columns:
-                    groupby_part = f" GROUP BY {grouping_columns}"
+                    groupby_part = f"GROUP BY {grouping_columns}"
 
                 for expr_set, function_set in current.aggregates.items():
                     for expr in expr_set:
@@ -130,13 +204,17 @@ class QueryGenerator:
             else:
                 select_part += ', ' + ', '.join(aggregation_functions)
 
-        where_part = " WHERE " + " AND ".join(where_conditions) if where_conditions else ""
+        where_part = "WHERE " + " AND ".join(where_conditions) if where_conditions else ""
         from_part += ", ".join([f"{relation.full_name} {relation.alias}" for relation in relations])
 
         if column_generator:
             additional_columns = column_generator(node, node.root())
             select_part += ', '.join([str(col) for col in additional_columns])
-        sql_query = f"{select_part} {from_part}{where_part}{groupby_part}"
+
+        if tab_line:
+            sql_query = f"{select_part}\n\t{from_part}\n\t{where_part}\n\t{groupby_part}"
+        else:
+            sql_query = f"{select_part}\n{from_part}\n{where_part}\n{groupby_part}"
         return sql_query.strip(), relations
 
     @staticmethod
@@ -145,12 +223,12 @@ class QueryGenerator:
         if insert_position == -1:
             insert_position = len(base_query)
 
-        new_query = base_query[:insert_position] + f" JOIN {join_clause} ON {on_condition} " + base_query[
-                                                                                               insert_position:]
+        new_query = base_query[:insert_position] + f"JOIN {join_clause} \nON {on_condition} \n" + base_query[
+                                                                                                  insert_position:]
         return new_query.strip()
 
     @staticmethod
-    def _rename_subquery_columns(subquery: str) -> str:
+    def _rename_subquery_columns(subquery: str, agg_mapping: dict) -> str:
         subquery_pattern = re.compile(r"\(SELECT (.*?) FROM .*?\)", re.IGNORECASE | re.DOTALL)
 
         def rename_columns(match):
@@ -158,15 +236,13 @@ class QueryGenerator:
             columns = [col.strip() for col in columns_part.split(',')]
 
             new_columns = []
-            unnamed_count = 0
 
             for col in columns:
-                if re.match(r'^\w+\.\w+$', col.strip()):
+                if col in agg_mapping.keys():
+                    new_columns.append(f"subquery.{agg_mapping[col]}")
+                else:
                     parts = col.split('.')
                     new_columns.append(f"subquery.{parts[1]}")
-                else:
-                    unnamed_count += 1
-                    new_columns.append(f"m{unnamed_count}")
 
             new_columns_part = ', '.join(new_columns)
             return f"{new_columns_part}"
